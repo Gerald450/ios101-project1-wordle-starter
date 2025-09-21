b3ffdb52cbd488d99a11907d1cb0b876772ed1a8
Gerald Shimo <shimogerald450@gmail.com>
2025-09-20 23:47:44 -0500

    add keyboard and click function and del function

--- Wordle/ViewController.swift
+++ Wordle/ViewController.swift
@@ -30,7 +30,17 @@ class ViewController: UIViewController {
       Else, it should use the `enter` method of `boardController` and pass in the selected string as the argument.
      */
     // START YOUR CODE HERE
-    // ...
+      keyboardController.didSelectString = { [weak self] str in
+          guard let self = self else {return}
+          if str == DELETE_KEY {
+              self.boardController.deleteLastCharacter()
+          }else{
+              self.boardController.enter(str)
+          }
+          
+          
+          
+      }
     // END YOUR CODE HERE
   }
 }
